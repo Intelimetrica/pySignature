8f670b806bffdc0c8f441fa579d2ea2e6ac120a2
Edgar Cabrera <ecabrera@intelimetrica.com>
2015-10-13 16:48:54 -0500

    Create project boilerplate with projy

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+""" PySignature setup.py script """
+
+# PySignature
+from pysignature import __version__
+
+# system
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from os.path import join, dirname
+
+
+setup(
+    name=__version__,
+    version='0.1.0',
+    description='My PySignature project',
+    author='Edgar Cabrera',
+    author_email='ecabrera@intelimetrica.com',
+    packages=['pysignature','pysignature.test'],
+    url='http://stephanepechard.github.com/projy',
+    long_description=open('README.txt').read(),
+    install_requires=[''],
+    test_suite='pysignature.test',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+      ],
+)
